88e4664eefdc47600662576de99d660cc36f3e1d
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-03-27 19:19:39 -0300

    feat: estilização sidebar

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,28 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css'
 import './global.css'
+
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Nílton Silva Ferreira" 
-        content="Lorem ipsum dolor sit amet consectetur adpisicing elit." 
-      />
-      <Post 
-        author="Silva Ferreira Nilton"
-        content="um novo post muito maneiro"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Nílton Silva Ferreira" 
+            content="Lorem ipsum dolor sit amet consectetur adpisicing elit." 
+          />
+          <Post 
+            author="Silva Ferreira Nilton"
+            content="um novo post muito maneiro"
+          />
+        </main>
+      </div>
     </div>
   )
 }
