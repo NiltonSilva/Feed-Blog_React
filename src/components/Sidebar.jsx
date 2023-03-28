1c675502d0d5ec22c98e26ec1dfe74c9205e8900
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-03-27 22:40:55 -0300

    feat: conclusão Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -5,15 +7,17 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img 
                 className={styles.cover} 
-                src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" 
+                src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" 
             />
             <div className={styles.profile}>
-                <strong>Nilton Silva</strong>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/51975928?v=4" />
+                <strong>Nílton Silva Ferreira</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
