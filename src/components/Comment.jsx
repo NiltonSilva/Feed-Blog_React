98a0d5368956ebb2f7a3a550262e520a29f20fdf
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-04-04 20:45:41 -0300

    feat: criado formulário e componente de comentários.

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
+import { ThumbsUp } from 'phosphor-react';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/51975928?v=4" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Nílton Silva Ferreira</strong>
+                            <time title="04 de Anril às 20:40h" dateTime="2023-04-04 20:40:00">Cerca de 1h atrás</time>
+                        </div>
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom, Nilton. Parabens!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
