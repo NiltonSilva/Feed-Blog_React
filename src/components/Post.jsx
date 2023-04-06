ddc124bee77bf8b6a4cee3e87e7a47361cfa2e7c
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-04-05 21:35:39 -0300

    feat: criando e estilizado o componente de avatar.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/51975928?v=4" alt="" />
+                    <Avatar src="https://avatars.githubusercontent.com/u/51975928?v=4" />
                     <div className={styles.authorInfo}>
                         <strong>Nílton Silva Ferreira</strong>
                         <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return  (
@@ -10,7 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" 
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/51975928?v=4" />
+                <Avatar src="http://avatars.githubusercontent.com/u/51975928?v=4" />
                 <strong>Nílton Silva Ferreira</strong>
                 <span>Web Developer</span>
             </div>
